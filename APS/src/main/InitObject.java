891512df82b0d7429df9a085e9f81dab60ba3dca
GuilhermeSP01 <gui.silvap01@gmail.com>
2024-05-10 20:41:59 -0300

    Randomização das coordenadas de spawn de lixo

--- APS/src/main/InitObject.java
+++ APS/src/main/InitObject.java
@@ -19,16 +19,28 @@ public class InitObject {
 	public Trash newObj() {
 		Random random = new Random();
 		int n = random.nextInt(4);
+		int coords = random.nextInt(2);
+		int x = 0, y = 0;
+		
+		switch(coords) {
+		case 0:
+			x = 12;
+			y = 4;
+			break;
+		case 1:
+			x = 12;
+			y = 8;
+		}
 		
 		switch(n) {
 		case 0:
-			return new Plastic(gp.tileSize * 12, gp.tileSize * 4);
+			return new Plastic(gp.tileSize * x, gp.tileSize * y);
 		case 1:
-			return new Paper(gp.tileSize * 12, gp.tileSize * 4);
+			return new Paper(gp.tileSize * x, gp.tileSize * y);
 		case 2:
-			return new Organic(gp.tileSize * 12, gp.tileSize * 4);
+			return new Organic(gp.tileSize * x, gp.tileSize * y);
 		case 3:
-			return new Glass(gp.tileSize * 12, gp.tileSize * 4);
+			return new Glass(gp.tileSize * x, gp.tileSize * y);
 		}
 		
 		return new Trash();
